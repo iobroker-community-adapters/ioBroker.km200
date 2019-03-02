4344428b60551c1653111e2404a855707aba5550
Thorsten Stueben <thorsten.stueben@macbook.local>
2019-03-02 16:53:20 +0100

    Translation

--- admin/words.js
+++ admin/words.js
@@ -2,42 +2,13 @@
 'use strict';
 
 systemDictionary = {
-    "KM200 adapter settings": {
-        "de": "Buderus KM200",
-        "ru": "Buderus KM200"
-    },
-    "address": {
-        "en": "Link to KM200",
-        "de": "KM200 Link",
-        "ru": "KM200 link"
-    },
-    "port": {
-        "en": "Port (80 default)",
-        "de": "Port (80 default)"
-    },
-    "blacklist": {
-        "en": 'list of names (can be parts with "*") to disable generating states from KM200 like: "/Gateway*", "/recordings/*',
-        'de': 'Liste der Namen (Können auch Teile sein mit "*") welche keine States vom KM200 generieren sollen wie: "/Gateway*", "/recordings/*'
-    },
-    "interval": {
-        "en": "Intrevall for polling (default 15 minutes, minimum 5 minutes)",
-        "de": "Intervall in Min (15 standard, 5 minimum)"
-    },
-    "accesskey": {
-        "en": "Accesskey generation on Webseite https://ssl-account.com/km200.andreashahn.info/",
-        "de": "Key, auf Webseite https://ssl-account.com/km200.andreashahn.info/ generiert!"
-    },
-    "Description": {
-        "en": "Please provide link to KM200 as ip addess or network name! On save adapter restarts with new config immediately",
-        "de": "Bitte Linkadresse angeben als ip-Adresse oder Netzwerkname angeben! Beim Speichern von Einstellungen wird der Adapter sofort neu gestartet.",
-        "ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями"
-    },
-    "Gateway Password": {
-        "en": "Device password",
-        "de": "Gerätepasswort"
-    },
-    "Private Password": {
-        "en": "Private password",
-        "de": "Privates Passwort"
-    }
+    "Description": {                                 "en": "Please provide link to KM200 as ip addess or network name! On save adapter restarts with new config immediately", "de": "Bitte Linkadresse angeben als ip-Adresse oder Netzwerkname angeben! Beim Speichern von Einstellungen wird der Adapter sofort neu gestartet.", "ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями", "pt": "Por favor, forneça o link para o KM200 como ip addess ou nome da rede! ", "nl": "Geef de link naar KM200 op als ip addess of netwerknaam! ", "fr": "Veuillez fournir un lien vers KM200 en tant qu'adresse IP ou nom de réseau! ", "it": "Fornire il collegamento a KM200 come indirizzo IP o nome di rete! ", "es": "Por favor, proporcione un enlace a KM200 como nombre de red o de IP! ", "pl": "Podaj link do KM200 jako adres IP lub nazwę sieci! ", "zh-cn": "请提供KM200作为ip addess或网络名称的链接！"},
+    "Gateway Password": {                            "en": "Device password",                                 "de": "Gerätepasswort",                                  "ru": "Пароль устройства",                               "pt": "Senha do dispositivo",                            "nl": "Apparaat wachtwoord",                             "fr": "Mot de passe de l'appareil",                      "it": "Password del dispositivo",                        "es": "Contraseña del dispositivo",                      "pl": "Hasło urządzenia",                                "zh-cn": "设备密码"},
+    "KM200 adapter settings": {                      "de": "Buderus KM200",                                   "ru": "Buderus KM200"},
+    "Private Password": {                            "en": "Private password",                                "de": "Privates Passwort",                               "ru": "Приватный пароль",                                "pt": "Senha privada",                                   "nl": "Privé wachtwoord",                                "fr": "Mot de passe privé",                              "it": "Password privata",                                "es": "Contraseña privada",                              "pl": "Prywatne hasło",                                  "zh-cn": "私人密码"},
+    "accesskey": {                                   "en": "Accesskey generation on Webseite https://ssl-account.com/km200.andreashahn.info/", "de": "Key, auf Webseite https://ssl-account.com/km200.andreashahn.info/ generiert!", "ru": "Создание ключа доступа на веб-сайте https://ssl-account.com/km200.andreashahn.info/", "pt": "Geração de Accesskey no site https://ssl-account.com/km200.andreashahn.info/", "nl": "Genereren van Accesskey op Webseite https://ssl-account.com/km200.andreashahn.info/", "fr": "Génération Accesskey sur Webseite https://ssl-account.com/km200.andreashahn.info/", "it": "Generazione di accesskey su Webseite https://ssl-account.com/km200.andreashahn.info/", "es": "Generación de la clave de acceso en el sitio web https://ssl-account.com/km200.andreashahn.info/", "pl": "Generowanie klucza dostępu w witrynie internetowej https://ssl-account.com/km200.andreashahn.info/", "zh-cn": "Webseite上的Accesskey生成https://ssl-account.com/km200.andreashahn.info/"},
+    "address": {                                     "en": "Link to KM200",                                   "de": "KM200 Link",                                      "ru": "KM200 link",                                      "pt": "Link para o KM200",                               "nl": "Link naar KM200",                                 "fr": "Lien vers KM200",                                 "it": "Collegamento a KM200",                            "es": "Enlace a KM200",                                  "pl": "Link do KM200",                                   "zh-cn": "链接到KM200"},
+    "blacklist": {                                   "en": "list of names (can be parts with \"*\") to disable generating states from KM200 like: \"/Gateway*\", \"/recordings/*", "de": "Liste der Namen (Können auch Teile sein mit \"*\") welche keine States vom KM200 generieren sollen wie: \"/Gateway*\", \"/recordings/*", "ru": "список имен (может состоять из частей с *) для отключения генерации состояний из KM200, например: \"/ Gateway *\", \"/ records / *", "pt": "lista de nomes (pode ser partes com \"*\") para desabilitar estados geradores do KM200 como: \"/ Gateway *\", \"/ recordings / *", "nl": "lijst met namen (kan delen zijn met \"*\") om generatorstatussen van KM200 uit te schakelen, zoals: \"/ Gateway *\", \"/ opnames / *", "fr": "liste de noms (pouvant être des parties avec \"*\") pour désactiver les états générateurs à partir de KM200 comme: \"/ Gateway *\", \"/ recordings / *", "it": "elenco di nomi (può essere parti con \"*\") per disabilitare gli stati di generazione da KM200 come: \"/ Gateway *\", \"/ registrazioni / *", "es": "lista de nombres (pueden ser partes con \"*\") para deshabilitar los estados generadores desde KM200 como: \"/ Gateway *\", \"/ recordings / *", "pl": "lista nazw (może być częściami z \"*\"), aby wyłączyć generowanie stanów z KM200 jak: \"/ Gateway *\", \"/ nagrania / *", "zh-cn": "名称列表（可以是带有“*”的部分）来禁用从KM200生成状态，如：“/ Gateway *”，“/ recordings / *"},
+    "interval": {                                    "en": "Intrevall for polling (default 15 minutes, minimum 5 minutes)", "de": "Intervall in Min (15 standard, 5 minimum)",       "ru": "Интервал для опроса (по умолчанию 15 минут, минимум 5 минут)", "pt": "Intrevall para pesquisa (padrão 15 minutos, mínimo 5 minutos)", "nl": "Intrevall voor polling (standaard 15 minuten, minimaal 5 minuten)", "fr": "Intrevall for polling (15 minutes par défaut, minimum 5 minutes)", "it": "Intrevall per il polling (predefinito 15 minuti, minimo 5 minuti)", "es": "Intrevall para sondeo (predeterminado 15 minutos, mínimo 5 minutos)", "pl": "Intrevall for polling (domyślnie 15 minut, minimum 5 minut)", "zh-cn": "用于投票的Intrevall（默认15分钟，最少5分钟）"},
+    "port": {                                        "en": "Port (80 default)",                               "de": "Port (80 default)",                               "ru": "Порт (80 по умолчанию)",                          "pt": "Porta (80 padrão)",                               "nl": "Poort (standaard 80)",                            "fr": "Port (80 par défaut)",                            "it": "Porta (80 predefinito)",                          "es": "Puerto (80 predeterminado)",                      "pl": "Port (domyślnie 80)",                             "zh-cn": "端口（默认为80）"},
 };
